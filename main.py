2f7744dd6b5616329ad75349d5b7d8ff0e1654f7
David Wood <dw@d4v1d.us>
2018-11-10 11:31:16 -0600

    working on register new account

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(10))
     password = db.Column(db.String(10))
-    email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(120), unique=True)
     blogz = db.relationship('Blog', backref='owner')
 
     def __init__(self, email, password):
@@ -44,43 +44,70 @@ def require_login():
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(email=email, password=password).first()
-        if user: #and check_pw_hash(password, user.pw_hash):
-            session['email'] = email
-            flash("Logged in", 'info')
-            return redirect('/')
+
+        if username == "" or password == "":
+            flash('Please enter your Login', 'danger')
+            return render_template('login.html', title="Blogz")
         else:
-            flash('User password incorrect, or user does not exist', 'danger')
-    return render_template('login.html', title="Build a Blog")
+            user = User.query.filter_by(username=username).first()
+            if not user:
+                flash('Username does not exist', 'danger')
+                return render_template('login.html', title="Blogz")
+
+        user = User.query.filter_by(username=username, password=password).first()
+
+        if user: #and check_pw_hash(password, user.pw_hash):
+            session['email'] = user.email
+            session['username'] = username
+            flash("Logged in", 'info')
+            return redirect('/newpost')
+        else:
+            flash('User Password incorrect', 'danger')
+            return render_template('login.html', title="Blogz", username=username)
+    return render_template('login.html', title="Blogz")
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
+        if username == None:
+            username = ''
+
+        #check username
+        if username == '':
+            flash("Hey Enter a Username!", 'danger')
+            return render_template('register.html')
+
+        if len(username) > 10 or len(username) < 3:
+            flash("Hey Username should be 3 to 10 in length!", 'danger')
+            return render_template('register.html')
+
+
         password = request.form['password']
         verify = request.form['verify']
 
         # TODO - validate user's data
 
-        existing_user = User.query.filter_by(email=email).first()
+        existing_user = User.query.filter_by(username=username).first()
         if not existing_user:
-            new_user = User(email, password)
+            new_user = User(username, password)
             db.session.add(new_user)
             db.session.commit()
-            session['email'] = email
+            session['username'] = username
             return redirect('/')
         else:
-            flash("The email <strong>{0}</strong> is already registered".format(email), 'danger')
+            flash("We already have the username <strong>{0}!!</strong>".format(username), 'danger')
 
     return render_template('register.html')
 
 @app.route('/logout', methods=['POST'])
 def logout():
     del session['email']
+    del session['username']
     return redirect('/')
-    
+
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
 
@@ -91,12 +118,12 @@ def newpost():
         if title == '':
 
             flash("You should enter a Title for you Post")
-            return render_template('newpost.html', title="Build a Blog", post_title=title, body=body)
+            return render_template('newpost.html', title="Blogz", post_title=title, body=body)
 
         if body == '':
 
             flash("You should enter the Body for you Post")
-            return render_template('newpost.html', title="Build a Blog", post_title=title, body=body)
+            return render_template('newpost.html', title="Blogz", post_title=title, body=body)
 
         new_post = Blog(title, body, owner)
 
@@ -105,7 +132,7 @@ def newpost():
 
         postID = new_post.id
         post = Blog.query.filter_by(id=postID).first()
-        return render_template('postdetail.html', title="Build a Blog", post=post)
+        return render_template('postdetail.html', title="Blogz", post=post)
 
         # posts = Blog.query.filter_by().all()
         # return render_template('blog.html', title="Build a Blog", posts=posts)
@@ -113,7 +140,7 @@ def newpost():
     if request.method == 'GET':
 
         #posts = Blog.query.filter_by().all()
-        return render_template('newpost.html', title="Build a Blog")
+        return render_template('newpost.html', title="Blogz")
 
 
 @app.route('/blog', methods=['POST', 'GET'])
@@ -123,10 +150,10 @@ def blog():
         postID = request.args.get('id', default=None)
         if postID != None:
             post = Blog.query.filter_by(id=postID).first()
-            return render_template('postdetail.html', title="Build a Blog", post=post)
+            return render_template('postdetail.html', title="Blogz", post=post)
         else:
             posts = Blog.query.filter_by().all()
-            return render_template('blog.html', title="Build a Blog", posts=posts)
+            return render_template('blog.html', title="Blogz", posts=posts)
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -143,12 +170,12 @@ def index():
 
         posts = Blog.query.filter_by().all()
 
-        return render_template('index.html', title="Build a Blog", posts=posts)
+        return render_template('index.html', title="Blogz", posts=posts)
 
     if request.method == 'GET':
 
         posts = Blog.query.filter_by().all()
-        return render_template('index.html', title="Build a Blog", posts=posts)
+        return render_template('index.html', title="Blogz", posts=posts)
 
 
 if __name__ == '__main__':
